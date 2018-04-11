08c3c723d85cfa11138a8b2c31719052954663cf
choueric <zhssmail@gmail.com>
2018-04-11 10:17:11 +0800

    update testcase

--- reggen_test.go
+++ reggen_test.go
@@ -20,17 +20,16 @@ const (
 
 <REG>: 1
   ck_prior1 : 0-1 
-  ck_prior2; 2-3
 
 <REG>: 0x10
 BWSEL_REG: 4-7`
 	trimedStr = `[  CHIP ] <chip>:si5324
 [  REG  ] <REG>[Control]: 0
-[ FIELD ] BYPASS_REG: 1
-[ FIELD ] CKOUT_ALWAYS_ON: 5
-[ FIELD ] FREE_RUN: 7 - 6
+[ FIELD ] BYPASS_REG: 1 ()
+[ FIELD ] CKOUT_ALWAYS_ON: 5 ()
+[ FIELD ] FREE_RUN: 7 - 6 ()
 [  REG  ] <REG>: 1
-[ FIELD ] ck_prior1 : 0-1
+[ FIELD ] ck_prior1 : 0-1 ()
 [  REG  ] <REG>: 0x10
 `
 	parsedStr = `CHIP: "si5324"
@@ -42,25 +41,37 @@ BWSEL_REG: 4-7`
     ck_prior1: [0:1]
 "16", 0x10
 `
-	formatCStr = `// Registers of si5324
+	formatCStr = `#pragma once
+
+#ifndef BIT
 #define BIT(x) (1 << (x))
+#endif
+
+// ONLY for _8bit-width_ register
 #define MASK(a, b) (((uint8_t)-1 >> (7-(b))) & ~((1U<<(a))-1))
 
-#define REG_CONTROL 0x0
+// Registers of si5324
+
+#define REG_CONTROL 0x0 // 0
 	#define REG_BYPASS_REG_BIT BIT(1)
 	#define REG_CKOUT_ALWAYS_ON_BIT BIT(5)
 	#define REG_FREE_RUN_MSK MASK(6, 7)
+	#define REG_FREE_RUN_VAL(rv) (((rv) & REG_FREE_RUN_MSK) >> 6)
+	#define REG_FREE_RUN_SFT(v) (((v) & REG_FREE_RUN_MSK) << 6)
 
-#define REG_1 0x1
+#define REG_1 0x1 // 1
 	#define REG_CK_PRIOR1_MSK MASK(0, 1)
+	#define REG_CK_PRIOR1_VAL(rv) ((rv) & REG_CK_PRIOR1_MSK)
+	#define REG_CK_PRIOR1_SFT(v) ((v) & REG_CK_PRIOR1_MSK)
 
-#define REG_16 0x10
+#define REG_16 0x10 // 16
 `
 )
 
 func print_string_mismatch(a, b []byte) {
 	if len(a) != len(b) {
 		fmt.Println("length doesn't match:", len(a), len(b))
+		// fmt.Println(string(a), "----------------------\n", string(b))
 		return
 	}
 	for i := 0; i < len(a) && i < len(b); i++ {
