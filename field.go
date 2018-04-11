1ca12e490aedf8ecc571520c436352698174514c
choueric <zhssmail@gmail.com>
2018-04-11 11:42:35 +0800

    implement parse of field's values part

--- field.go
+++ field.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"regexp"
@@ -11,19 +12,46 @@ import (
 )
 
 type field struct {
-	name  string
-	start uint32
-	end   uint32
+	name    string
+	start   uint32
+	end     uint32
+	valData []uint32
+	valName []string
 }
 
 func (f *field) String() string {
-	return fmt.Sprintf("%s: [%d:%d]", f.name, f.start, f.end)
+	var str bytes.Buffer
+	fmt.Fprintf(&str, "%s: [%d:%d]", f.name, f.start, f.end)
+	fmt.Fprintf(&str, " (")
+	for i, v := range f.valData {
+		fmt.Fprintf(&str, "%d: %s, ", v, f.valName[i])
+	}
+	fmt.Fprintf(&str, ")")
+	return str.String()
 }
 
-// validate the field format like `name: offset (val: v, val:v)`
+// `0b000111` to uint32
+func parseBinStr(str string) (uint32, error) {
+	if v, err := strconv.ParseUint(str[2:], 2, 32); err != nil {
+		return 0, err
+	} else {
+		return uint32(v), nil
+	}
+}
+
+// base 16`0x`, base 8`0` and base 10 to uint32
+func parseOtherBaseStr(str string) (uint32, error) {
+	if v, err := strconv.ParseUint(str, 0, 32); err != nil {
+		return 0, err
+	} else {
+		return uint32(v), nil
+	}
+}
+
+// validate the field format like `name: offset (val: vname, val:vname)`
 // which contains two parts:
 //   - nameOffset, i.e. `name: offset`
-//   - values, i.e. `(val: v, val:v)`. This part is optional
+//   - values, i.e. `(val: vname, val:vname)`. This part is optional
 // If format is not correct, return (nil, false)
 // Otherwise, return ({str of nameOffset, str of values}, true)
 func validField(line string) ([]string, bool) {
@@ -91,6 +119,50 @@ func processFiledNameOffset(f *field, nameOffset string) error {
 }
 
 func processFiledValues(f *field, valStr string) error {
+	if valStr == "" {
+		return nil
+	}
+
+	nameStrs := []string{}
+	valStrs := []string{}
+
+	addPair := func(str string) {
+		pair := strings.Split(str, ":")
+		if len(pair) != 2 {
+			clog.Fatal("Invalid field value format: " + valStr)
+		}
+		valStrs = append(valStrs, strings.TrimSpace(pair[0]))
+		nameStrs = append(nameStrs, strings.TrimSpace(pair[1]))
+	}
+
+	strs := strings.Split(valStr, ",")
+	if len(strs) == 1 { // may contain single pair
+		addPair(valStr)
+	} else {
+		for _, pairStr := range strs {
+			addPair(pairStr)
+		}
+	}
+
+	parseFunc := map[bool]func(string) (uint32, error){
+		true:  parseBinStr,
+		false: parseOtherBaseStr,
+	}
+
+	for i, ns := range nameStrs {
+		matched, err := regexp.MatchString(`^0b[01]*`, valStrs[i])
+		if err != nil {
+			clog.Fatal(err)
+		}
+
+		if v, err := parseFunc[matched](valStrs[i]); err != nil {
+			clog.Fatal(err)
+		} else {
+			f.valData = append(f.valData, v)
+			f.valName = append(f.valName, ns)
+		}
+	}
+
 	return nil
 }
 
