bb7c635a1d61da6135318c04d0c2b747265c3499
choueric <zhssmail@gmail.com>
2018-04-16 13:50:21 +0800

    test: update test case

--- reggen_test.go
+++ reggen_test.go
@@ -63,33 +63,33 @@ const (
 	#define REG_FREE_RUN_END 7
 	#define REG_FREE_RUN_MSK MASK(6, 7)
 	#define REG_FREE_RUN_VAL(rv) (((rv) & REG_FREE_RUN_MSK) >> 6)
-	#define REG_FREE_RUN_SFT(v) (((v) & REG_FREE_RUN_MSK) << 6)
+	#define REG_FREE_RUN_SFT(v) (((v) & MASK(0, 1)) << 6)
 	#define REG_CK_PRIOR1_STR 0
 	#define REG_CK_PRIOR1_END 1
 	#define REG_CK_PRIOR1_MSK MASK(0, 1)
 	#define REG_CK_PRIOR1_VAL(rv) ((rv) & REG_CK_PRIOR1_MSK)
-	#define REG_CK_PRIOR1_SFT(v) ((v) & REG_CK_PRIOR1_MSK)
+	#define REG_CK_PRIOR1_SFT(v) ((v) & MASK(0, 1))
 
 #define REG_16 0x10 // 16
 	#define REG_BWSEL_REG_STR 4
 	#define REG_BWSEL_REG_END 7
 	#define REG_BWSEL_REG_MSK MASK(4, 7)
 	#define REG_BWSEL_REG_VAL(rv) (((rv) & REG_BWSEL_REG_MSK) >> 4)
-	#define REG_BWSEL_REG_SFT(v) (((v) & REG_BWSEL_REG_MSK) << 4)
+	#define REG_BWSEL_REG_SFT(v) (((v) & MASK(0, 3)) << 4)
 
 #define REG_FIELD_VALS 0x11 // 17
 	#define REG_FOS_STR 5
 	#define REG_FOS_END 6
 	#define REG_FOS_MSK MASK(5, 6)
 	#define REG_FOS_VAL(rv) (((rv) & REG_FOS_MSK) >> 5)
-	#define REG_FOS_SFT(v) (((v) & REG_FOS_MSK) << 5)
+	#define REG_FOS_SFT(v) (((v) & MASK(0, 1)) << 5)
 		#define REG_FOS_FOS_0	0	// 0b0	0x0
 		#define REG_FOS_FOS_3	3	// 0b11	0x3
 	#define REG_VALTIME_STR 3
 	#define REG_VALTIME_END 4
 	#define REG_VALTIME_MSK MASK(3, 4)
 	#define REG_VALTIME_VAL(rv) (((rv) & REG_VALTIME_MSK) >> 3)
-	#define REG_VALTIME_SFT(v) (((v) & REG_VALTIME_MSK) << 3)
+	#define REG_VALTIME_SFT(v) (((v) & MASK(0, 1)) << 3)
 		#define REG_VALTIME_0MS	0	// 0b0	0x0
 		#define REG_VALTIME_1MS	1	// 0b1	0x1
 		#define REG_VALTIME_2MS	2	// 0b10	0x2
@@ -98,7 +98,7 @@ const (
 	#define REG_LOCKT_END 2
 	#define REG_LOCKT_MSK MASK(0, 2)
 	#define REG_LOCKT_VAL(rv) ((rv) & REG_LOCKT_MSK)
-	#define REG_LOCKT_SFT(v) ((v) & REG_LOCKT_MSK)
+	#define REG_LOCKT_SFT(v) ((v) & MASK(0, 2))
 		#define REG_LOCKT_0T	0	// 0b0		0x0
 		#define REG_LOCKT_5T	10	// 0b1010	0xa
 
@@ -107,7 +107,7 @@ const (
 	#define REG_SINGLE_END 3
 	#define REG_SINGLE_MSK MASK(2, 3)
 	#define REG_SINGLE_VAL(rv) (((rv) & REG_SINGLE_MSK) >> 2)
-	#define REG_SINGLE_SFT(v) (((v) & REG_SINGLE_MSK) << 2)
+	#define REG_SINGLE_SFT(v) (((v) & MASK(0, 1)) << 2)
 		#define REG_SINGLE_TWO	2	// 0b10	0x2
 `
 )
